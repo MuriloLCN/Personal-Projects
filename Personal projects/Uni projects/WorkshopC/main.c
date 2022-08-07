5e34a059568a59a76a4e2e1e60ddab8db51c2ef4
MuriloLCN <88753590+MuriloLCN@users.noreply.github.com>
2022-08-07 19:25:51 -0300

    Semana 2

--- Personal projects/Uni projects/WorkshopC/main.c
+++ Personal projects/Uni projects/WorkshopC/main.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
 void pegar(char mensagem[200], int *ptr, char modo[2]) {
     printf("%s", mensagem);
 
-    scanf(modo, &*ptr);
+    if (modo == "%s") {
+        scanf(modo, *ptr);
+    }
+    else {
+        scanf(modo, &*ptr);
+    }
 
     printf("\n");
 }
+*/
 
 void main()
 {
-    printf("-------------------------\nSupermercado C\n-------------------------\n\n");
-
     float precoPacoteDeArroz = 12.5;
     float precoPacoteDeFeijao = 8;
     float precoCaixaDeOvos = 6.5;
@@ -21,8 +26,96 @@ void main()
 
     float valorCompra, troco, valorRecebido;
 
-    int qtdArroz, qtdFeijao, qtdOvos, qtdRefrigerante, qtdSalgadinho;
+    int qtdArroz = 0, qtdFeijao = 0, qtdOvos = 0, qtdRefrigerante = 0, qtdSalgadinho = 0;
 
+    int temp;
+
+    float subTotal = 0;
+
+    while (1){
+        system("cls");
+        printf("-------------------------\nSupermercado C\n-------------------------\n\n");
+        printf("Qual item voce deseja comprar?\n");
+        printf("[1] Arroz       (preco: %.2f) | No carrinho: %d\n", precoPacoteDeArroz, qtdArroz);
+        printf("[2] Feijao       (preco: %.2f) | No carrinho: %d\n", precoPacoteDeFeijao, qtdFeijao);
+        printf("[3] Ovos         (preco: %.2f) | No carrinho: %d\n", precoCaixaDeOvos, qtdOvos);
+        printf("[4] Refrigerante (preco: %.2f) | No carrinho: %d\n", precoRefrigerante, qtdRefrigerante);
+        printf("[5] Salgadinho   (preco: %.2f) | No carrinho: %d\n", precoSalgadinho, qtdSalgadinho);
+        printf("Sub total: %.2f\n", subTotal);
+        printf("Insira [0] para finalizar suas compras, ou o negativo do numero do item para retirar um\n");
+
+        scanf("%d", &temp);
+        fflush(stdin);
+
+        int mult = 1;
+
+        if (temp == 0) {
+            printf("\nSeu subtotal: %.2f", subTotal);
+            printf("\nInsira o valor que ira pagar: R$");
+            scanf("%f", &valorRecebido);
+            fflush(stdin);
+            troco = valorRecebido - subTotal;
+            if (troco < 0) {
+                printf("\nVoce nao tem dinheiro suficiente para cobrir essa transacao, tente retirar algum item do carrinho\n");
+                printf("[0] Cancelar transacao mesmo assim\n[Outro valor] Voltar a tela de compras\n");
+                scanf("%d", &temp);
+                fflush(stdin);
+                if (temp == 0) {
+                    break;
+                }
+            } else {
+                printf("\n-------------\nObrigado pela preferencia\nSeu troco: R$%.2f", troco);
+                break;
+            }
+        }
+        if (temp < 0) {
+            temp *= -1;
+            mult *= -1;
+        }
+
+        switch (temp) {
+            case 1:
+                if (qtdArroz <= 0 && mult == -1) {
+                    break;
+                }
+                subTotal += precoPacoteDeArroz * mult;
+                qtdArroz += 1 * mult;
+                break;
+            case 2:
+                if (qtdFeijao <= 0 && mult == -1) {
+                    break;
+                }
+                subTotal += precoPacoteDeFeijao * mult;
+                qtdFeijao += 1 * mult;
+                break;
+            case 3:
+                if (qtdOvos <= 0 && mult == -1) {
+                    break;
+                }
+                subTotal += precoCaixaDeOvos * mult;
+                qtdOvos += 1 * mult;
+                break;
+            case 4:
+                if (qtdRefrigerante <= 0 && mult == -1) {
+                    break;
+                }
+                subTotal += precoRefrigerante * mult;
+                qtdRefrigerante += 1 * mult;
+                break;
+            case 5:
+                if (qtdSalgadinho <= 0 && mult == -1) {
+                    break;
+                }
+                subTotal += precoSalgadinho * mult;
+                qtdSalgadinho += 1 * mult;
+                break;
+        }
+    }
+
+    printf("\nTotal da compra: %.2f", subTotal);
+
+
+    /*
     printf("Insira o numero de itens comprados\n-------------------------\n");
 
     printf("Preco arroz: %.2f\n", precoPacoteDeArroz);
@@ -46,15 +139,5 @@ void main()
     valorCompra += precoCaixaDeOvos * qtdOvos;
     valorCompra += precoRefrigerante * qtdRefrigerante;
     valorCompra += precoSalgadinho * qtdSalgadinho;
-
-    printf("-------------------------\nValor total da compra: R$%.2f\n", valorCompra);
-
-    pegar("Insira o valor pago: R$", &valorRecebido, "%f");
-
-    troco = valorRecebido - valorCompra;
-
-    printf("Troco: R$%.2f\n", troco);
-
-    printf("\n-------------------------\nObrigado pela preferencia, tenha um bom dia!\n-------------------------\n");
-
+    */
 }
